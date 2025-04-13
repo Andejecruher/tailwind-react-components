b23b8b5480c82075e9973d341042905509b82a07
Antonio Cruz Hernandez <antonio.cruz@clupp.com.mx>
2025-04-13 00:02:09 -0600

    feat: actualizar el enrutador para usar RootLayout y Home como componentes principales

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom"
-import App from "../App" // Layout principal si lo usas
+import RootLayout from "@src/components/layout"
+import Home from "@src/pages/Home"
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />, // App como layout base si deseas
+        element: <RootLayout />, // App como layout base si deseas
         children: [
-            { index: true, element: <div className="text-2xl">Home</div> },
+            { index: true, element: <Home /> },
             { path: "buttons", element: <div className="text-2xl">Aqui van los botones</div> },
             { path: "cards", element: <div className="text-2xl">Aqui van las tarjetas</div> },
             { path: "about", element: <div className="text-2xl">About Us</div> },
