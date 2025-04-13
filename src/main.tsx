38a0ba5fce0ed0b34d0a34770039def1cf13b3b9
Antonio Cruz Hernandez <antonio.cruz@clupp.com.mx>
2025-04-13 00:02:41 -0600

    feat: eliminar componente App y ajustar la estructura de main.tsx para incluir ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from "@src/context/theme-provider"
 import router from './router/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 )
